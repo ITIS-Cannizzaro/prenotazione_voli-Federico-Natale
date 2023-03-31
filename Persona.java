f76f001ce442c6e6b38b4ee769ae5f8c02f33530
Federico-Natale <125025861+Federico-Natale@users.noreply.github.com>
2023-03-31 11:43:52 +0200

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,11 @@
+public abstract class Persona 
+{   
+	String Nome, Cognome;
+	public Persona(String nome, String cognome) 
+	{
+		Nome=nome;
+		Cognome=cognome;
+	}
+	
+	 abstract String generalita();
+	}
